a731699c1beb320ea7bef7f2b32e2fac67c4b03f
davidcba01 <davoocordoba2001@gmail.com>
2026-06-02 13:40:35 -0300

    Se avanzo en el ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Hello, World!");
 int a;
 int b;
 a = 10;
 b = a;
 
 Console.WriteLine($"valor de a = {a}");
-Console.WriteLine($"valor de b = {b}");
+Console.WriteLine($"valor de b = {b}");*/
+
+// -------------Ejercicio 1)-------------
+int num, digito, aux;
+
+Console.WriteLine("Ingrese el numero que desea invertir");
+num = Console.Read();
+
+if (num>0)
+{
+    while (num != 0)
+    {
+        
+    }
+}
