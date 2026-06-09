f5d73d497b4f73eae4d171d7c7675882aa711e5f
LukaCaceres <lukadayo03@gmail.com>
2026-06-09 12:45:55 -0300

    Se termino el primer ejercicio

--- Program.cs
+++ Program.cs
@@ -9,15 +9,28 @@ Console.WriteLine($"valor de a = {a}");
 Console.WriteLine($"valor de b = {b}");*/
 
 // -------------Ejercicio 1)-------------
-int num, digito, aux;
-
+int num, digito, numeroFinal=0;
+string cadena;
 Console.WriteLine("Ingrese el numero que desea invertir");
-num = Console.Read();
-
-if (num>0)
+cadena = Console.ReadLine();
+if (int.TryParse(cadena, out num))
 {
-    while (num != 0)
+    if (num>0)
     {
-        
+        while (num != 0)
+        {
+            numeroFinal *= 10;
+            digito = num % 10;
+            numeroFinal += digito;
+            num /= 10;
+        }
+        Console.WriteLine($"{numeroFinal}");
+    }else
+    {
+        Console.WriteLine("Lo que ingreso no es valido");
     }
+}else
+{
+    Console.WriteLine("Lo que ingreso no es valido");
 }
+
