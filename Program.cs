21a02fcc121b2a65016eacae926c37a6a94e716d
sebastianvellicce <svellicceleal@gmail.com>
2026-06-27 14:40:14 -0300

    Se hizo la segunda version de la calculadora

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ void mostrarMenu()
     Console.WriteLine("4-Dividir\n");
     Console.WriteLine("5-Salir\n");
 }
-int opcion, num1, num2, resultado;
-string cadena, cadenaNum1, cadenaNum2;
+int opcion; 
+float num1, num2, resultado, numUnico, numMax1,numMax2;
+string cadena, cadenaNum1, cadenaNum2, cadenaNumUnico,cNumMax1,cNumMax2;
 
 mostrarMenu();
 
@@ -27,7 +28,7 @@ while (opcion != 5)
         Console.WriteLine("Ingrese el segundo numero\n");
         cadenaNum2 = Console.ReadLine();
 
-        if (int.TryParse(cadenaNum1, out num1) && int.TryParse(cadenaNum2, out num2))
+        if (float.TryParse(cadenaNum1, out num1) && float.TryParse(cadenaNum2, out num2))
         {
             switch (opcion)
             {
@@ -69,7 +70,30 @@ while (opcion != 5)
     {
         Console.WriteLine("Opcion invalida\n");
     }
-
+    Console.WriteLine("Ingrese un numero unico\n");
+    cadenaNumUnico = Console.ReadLine();
+    if (float.TryParse(cadenaNumUnico, out numUnico))
+    {
+        Console.WriteLine($"Valor absoluto = {Math.Abs(numUnico)}\n");
+        Console.WriteLine($"Numero al cuadrado = {Math.Pow(numUnico, 2)}\n");
+        if (numUnico >= 0)
+        {
+            Console.WriteLine($"Raiz cuadrada = {Math.Sqrt(numUnico)}\n");
+        }
+        Console.WriteLine($"Seno = {Math.Sin(numUnico)}\n");
+        Console.WriteLine($"Coseno = {Math.Cos(numUnico)}\n");
+        Console.WriteLine($"Parte entera = {Math.Truncate(numUnico)}\n");
+    }
+    Console.WriteLine("primer numero\n");
+    cNumMax1 = Console.ReadLine();
+    Console.WriteLine("segundo numero\n");
+    cNumMax2 = Console.ReadLine();
+    if(float.TryParse(cNumMax1, out numMax1) && float.TryParse(cNumMax2, out numMax2))
+    {
+        Console.Write($"El numero maximo entre los dos ingresados es {Math.Max(numMax1, numMax2)}\n");
+    }
+    
+    
     mostrarMenu();
     Console.WriteLine("Ingrese una opcion");
     cadena = Console.ReadLine();
